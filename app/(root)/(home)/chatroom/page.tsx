1888a3f39939cc91b744ac7a92d381d6704823af
Adriz1611 <adrijbhadra@gmail.com>
2024-04-20 06:54:21 +0530

    yelelo

--- app/(root)/(home)/chatroom/page.tsx
+++ app/(root)/(home)/chatroom/page.tsx
@@ -2,7 +2,7 @@
 const PreviousPage = () => {
 	return (
 	  <section className="flex size-full flex-col gap-10 text-white">
-		<h1 className="text-3xl font-bold">Recordings</h1>
+		<h1 className="text-3xl font-bold">Chatroom</h1>
 		  <a className="inline-block bg-transparent border border-gray-700 font-semibold py-2 px-4 rounded text-white mt-4 lg:mt-0 w-auto" href='https://www.google.com'>Continue to chatroom</a>
 	  </section>
 	);
